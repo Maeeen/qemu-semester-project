3825eccf10f3f8cd1c5223db8be4bd6e814dc981
Marwan Azuz <marwanazuz+cinnabyte@gmail.com>
2024-10-28 11:43:15 +0100

    benchmark inscount

--- benchmarks/inscount/tblog.c
+++ benchmarks/inscount/tblog.c
@@ -3,22 +3,24 @@
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
-size_t icount = 0;
 
 void insn_exec(unsigned int vcpu_index, void *userdata) {
-  icount += (size_t) userdata;
+  printf("{\"type\": \"exec\", \"n_insn\": %zu},", (size_t) userdata);
 }
 
 void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
   size_t n_insn = qemu_plugin_tb_n_insns(tb);
+  printf("{\"type\": \"trans\", \"n_insn\": %zu},", n_insn);
   qemu_plugin_register_vcpu_tb_exec_cb(tb, insn_exec, QEMU_PLUGIN_CB_NO_REGS, (void*) n_insn);
 }
 
 void when_exit(qemu_plugin_id_t id, void *userdata) {
-  printf("Instruction count: %zu\n", icount);
+  printf("{}]\n");
+  fflush(stdout);
 }
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info, int argc, char **argv) {
+  printf("[");
   qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
   qemu_plugin_register_atexit_cb(id, when_exit, NULL);
   return 0;
